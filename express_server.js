e4f68dbb993fb451c2eeb6ed40a05c7da58030dd
kineret.braslavsky <kineret.braslavsky@gmail.com>
2020-12-08 07:07:02 +0000

    commit changes

--- express_server.js
+++ express_server.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -22,3 +39,12 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
